30db6e140146997dfa3f3306cf8b258f191cafcc
bayron meneses <baironmenesesidarraga.990128@gmail.com>
2025-04-04 22:07:12 -0500

    Primera etapa del bot general de cartmots

--- app/Http/Controllers/Wpp/WppController.php
+++ app/Http/Controllers/Wpp/WppController.php
@@ -736,7 +736,7 @@ class WppController extends Controller
         
         /* bot general */
         Log::info("Empresa es = $empresas   ");
-        if($empresas !== 8){
+        if($empresas != 8){
             $messageId = contactos_chat::where("telefono",$telefono)->where('empresas',$empresas)->first();
             if($messageId->mensaje1 === 0 ){
                 $messageId->mensaje1 = 1;
