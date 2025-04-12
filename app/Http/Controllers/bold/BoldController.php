a4a57c7d9c1cba6b4e40a52a90b5c837d6d98491
bayron meneses <baironmenesesidarraga.990128@gmail.com>
2025-04-12 16:51:56 -0500

    Se agrega la consulta de los metodos de pago de bold

--- app/Http/Controllers/bold/BoldController.php
+++ app/Http/Controllers/bold/BoldController.php
@@ -7,13 +7,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class BoldController extends Controller {
-    public $llavePriv = "aCGtzBEW1B0y78_7QVfU5O9OOuWSu4wP8DA3c2G-uPk";
+    public $llavePriv = "RpYgEC4H3YxdigNF-sfcP0rLmtJ1I04Frv3XmPEEIUc";
     public $baseUrl = "https://integrations.api.bold.co";
     /* consulta los metodos de pago  */
     function metodosPago(Request $request){
         $key = $this->llavePriv;
         $data = Http::withHeaders([
-            'Autorization'=>"x-api-key $key",
+            'Authorization'=>"x-api-key $key",
             'Accept' => 'application/json',
         ])->get($this->baseUrl.'/payments/payment-methods');
         return response()->json($data->json());
