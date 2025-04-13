10f0993bd22a6b06a6d8e4783bc3d3f5cb942750
bayron meneses <baironmenesesidarraga.990128@gmail.com>
2025-04-13 10:32:01 -0500

    Se agrega la consulta de los metodos de pago de bold

--- app/Http/Controllers/Paypal/PaypalController.php
+++ app/Http/Controllers/Paypal/PaypalController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Http;
 
 class PaypalController extends Controller
 {
-    public $apiKey = "TEST-a194d62e-4b7a-444a-94cd-ce3b2be7e791";
+    public $apiKey = "TEST-1085150894423410-030803-ea4be12ca2083d3a93f496874831507f-507411332";
     public $baseUrl = "https://api.mercadopago.com/v1";
 
     function metodosPago()
