50742c32d9107a7d3b4c38d386bbd9d938702044
bayron meneses <baironmenesesidarraga.990128@gmail.com>
2025-04-13 12:01:59 -0500

    Se agrega la consulta de los metodos de pago de bold

--- app/Http/Controllers/Paypal/PaypalController.php
+++ app/Http/Controllers/Paypal/PaypalController.php
@@ -29,57 +29,13 @@ class PaypalController extends Controller
         $uuid = Str::uuid();
         
         $data = [
-        "additional_info" => [
-            "items" => [
-                [
-                    "id" => rand(1,100000).'cartmots',
-                    "title" => "Cartmots mes",
-                    "description" => "Pago de mensualidad de uso de la plataforma cartmots",
-                    "picture_url" => "https://public.cartmots.com/storage/AUTOSSELECCIONADOS/logos//JNe29OBusTtrEKonazdFlIqQrgzsQwzjN4Tj5k6N.jpg",
-                    "category_id" => "electronics",
-                    "quantity" => 1,
-                    "unit_price" => 350000,
-                    "type" => "electronics",
-                    "event_date" => Carbon::now(),
-                    "warranty" => false,
-                    "category_descriptor" => [
-                        "passenger" => [],
-                        "route" => [],
-                    ],
-                ],
-            ],
-            "payer" => [
-                "first_name" => "bayron",
-                "last_name" => "",
-                "phone" => [
-                    "area_code" => 57,
-                    "number" => "3184482848",
-                ]
-            ],
+        "installments"=>1,
+        "payer"=>[
+            "email"=>"baironmenesesidarraga.990128@gmail.com",
+
         ],
-        "application_fee" => null,
-        "binary_mode" => false,
-        "campaign_id" => null,
-        "capture" => false,
-        "coupon_amount" => null,
-        "description" => "Payment for product",
-        "differential_pricing_id" => null,
-        "external_reference" => null,
-        "installments" => 1,
-        "metadata" => null,
-        "payer" => [
-            "entity_type" => "individual",
-            "type" => "customer",
-            "id" => null,
-            "email" => "baironmenesesidarraga.990128@gmail.com",
-            "identification" => [
-                "type" => "CC",
-                "number" => "1143994831",
-            ],
-        ],
-        "payment_method_id" => $request['metodo'],
-        "token" => "$uuid",
-        "transaction_amount" => 350000,
+        "token"=>$request['token'],
+        "transaction_amount"=>350000,
     ];
     
         $key = $this->apiKey;
