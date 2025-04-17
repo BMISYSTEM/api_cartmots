6288317f5f350cce20bb8f968e09c011cff71cc9
bayron meneses <baironmenesesidarraga.990128@gmail.com>
2025-04-16 21:33:31 -0500

    prueba de integracion bold

--- app/Http/Controllers/bold/BoldController.php
+++ app/Http/Controllers/bold/BoldController.php
@@ -33,7 +33,7 @@ class BoldController extends Controller {
         $currentNanoseconds = microtime(true) * 1e9; // Convertir microsegundos a nanosegundos
         $tenMinutesInNanoseconds = 10 * 60 * 1e9; // 10 minutos en nanosegundos
         $futureNanoseconds = $currentNanoseconds + $tenMinutesInNanoseconds;
-        $data = [
+        $array = [
             "amount_type"=>"CLOSE",
             "amount" => [
                 "currency" => "COP",
@@ -47,13 +47,31 @@ class BoldController extends Controller {
             "payer_email"=>"baironmenesesidarraga.990128@gmail.com",
 
         ];
-        
+        $datos = [
+                "amount_type" => "CLOSE",
+                "amount"=> [
+                 "currency"=>"COP",
+                 "taxes"=> [
+                   [
+                     "type"=> "VAT",
+                     "base"=>8403,
+                     "value"=> 1597
+                 ]
+                 ],
+                 "tip_amount"=> 0,
+                 "total_amount"=> 10000
+                ],
+               "description"=> "Mi descripción del producto o servicio",
+               "expiration_date"=>1798598454489,
+               "payment_methods"=> [
+                      "PSE"
+                ],
+               "image_url"=> "https://robohash.org/sad.png"
+            ];
         $data = Http::withHeaders([
             'Authorization'=>"x-api-key $key",
             'Accept' => 'application/json',
-        ])->post($this->baseUrl.$endpoint,[
-
-        ]);
+        ])->post($this->baseUrl.$endpoint,$array);
         return response()->json($data->json());
     }
 }
