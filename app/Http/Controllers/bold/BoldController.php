b174ae14afbe54b11ad59ea51b13df3e6d416751
bayron meneses <baironmenesesidarraga.990128@gmail.com>
2025-04-16 22:03:44 -0500

    se genero el link correctamente

--- app/Http/Controllers/bold/BoldController.php
+++ app/Http/Controllers/bold/BoldController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\bold;
 
 use App\Http\Controllers\Controller;
+use App\Models\factura;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 
 class BoldController extends Controller {
@@ -33,45 +35,29 @@ class BoldController extends Controller {
         $currentNanoseconds = microtime(true) * 1e9; // Convertir microsegundos a nanosegundos
         $tenMinutesInNanoseconds = 10 * 60 * 1e9; // 10 minutos en nanosegundos
         $futureNanoseconds = $currentNanoseconds + $tenMinutesInNanoseconds;
+        $empresas = Auth::user()->empresas;
+        $factura = factura::where('empresas',$empresas)->where('estado',0)->get();
         $array = [
             "amount_type"=>"CLOSE",
             "amount" => [
                 "currency" => "COP",
                 "tip_amount" => 0,
-                "total_amount" => 5000
+                "total_amount" => $factura[0]->valor
             ],
             "expiration_date"=>$futureNanoseconds,
             "payment_method" => ["POS"],
-            "description"=>"Pago de soporte CRM cartmots",
+            "description"=>$factura->descrpcion,
             "callback_url"=>"https://cartmots.com/panel/dashboard",
             "payer_email"=>"baironmenesesidarraga.990128@gmail.com",
 
         ];
-        $datos = [
-                "amount_type" => "CLOSE",
-                "amount"=> [
-                 "currency"=>"COP",
-                 "taxes"=> [
-                   [
-                     "type"=> "VAT",
-                     "base"=>8403,
-                     "value"=> 1597
-                 ]
-                 ],
-                 "tip_amount"=> 0,
-                 "total_amount"=> 10000
-                ],
-               "description"=> "Mi descripción del producto o servicio",
-               "expiration_date"=>1798598454489,
-               "payment_methods"=> [
-                      "PSE"
-                ],
-               "image_url"=> "https://robohash.org/sad.png"
-            ];
+       
         $data = Http::withHeaders([
             'Authorization'=>"x-api-key $key",
             'Accept' => 'application/json',
         ])->post($this->baseUrl.$endpoint,$array);
-        return response()->json($data->json());
+        $res = $data->json();
+        $factura->idLink = $res['payload']['payment_link'];
+        return response()->json($res);
     }
 }
