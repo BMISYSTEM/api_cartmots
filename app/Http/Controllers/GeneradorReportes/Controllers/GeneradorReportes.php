dead38e2b7432145049e12c498deac9964a43636
bayron meneses <baironmenesesidarraga.990128@gmail.com>
2025-04-18 15:04:23 -0500

    Reporteria

--- app/Http/Controllers/GeneradorReportes/Controllers/GeneradorReportes.php
+++ app/Http/Controllers/GeneradorReportes/Controllers/GeneradorReportes.php
@@ -1,16 +1,19 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\GeneradorReportes\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class GeneradorReportes extends Controller
 {
+    /* consulta sql libre  */
     public function ConsultaSQL(Request $request)
     {
-        $consulta = DB::select($request['sql']);
+        $consultaSql = $request['sql'];
+        $consulta = DB::select($consultaSql);
         return response()->json($consulta);
     }
 }
